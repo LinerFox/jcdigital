0723805a8668675bcd7b39c1e3cd72b9b206b4c4
Joseph Chu <karkinoscode@gmail.com>
2022-10-16 16:05:07 -0400

    Finished About Section

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -13,10 +14,13 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       {/* Hero */}
-      <section id='hero' className='snap-center'>
+      <section id='hero' className='snap-start'>
         <Hero />
       </section>
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
