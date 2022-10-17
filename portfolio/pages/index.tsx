d1a1b4bce23e602c7ceb3c678c161421161f4900
Joseph Chu <karkinoscode@gmail.com>
2022-10-17 13:58:19 -0400

    Finished Projects section

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import type { NextPage } from "next";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
@@ -24,11 +25,13 @@ const Home: NextPage = () => {
       <section id='experience' className='snap-center'>
         <Experience />
       </section>
-      {/* Skills */}
       <section id='skills' className='snap-start'>
         <Skills />
       </section>
       {/* Projects */}
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
       {/* Contact */}
     </div>
   );
