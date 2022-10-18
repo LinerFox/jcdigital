5323830fb9c25398bbf54db23ba224c4e71d871e
Joseph Chu <karkinoscode@gmail.com>
2022-10-18 12:52:51 -0400

    Updated Github Actions yaml file

--- portfolio/components/Contact.tsx
+++ portfolio/components/Contact.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 function Contact({}: Props) {
-  return <div className='H-SCREEN'>Contact</div>;
+  return <div className='h-screen'>Contact</div>;
 }
 
 export default Contact;
